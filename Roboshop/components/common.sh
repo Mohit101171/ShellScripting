a33c46d08e0feec526b8580fd07dc38c2f3c09e7
Mohit Kumar <mk.singh0246@gmail.com>
2024-09-08 01:43:16 +0530

    corrected fn in common.sh

--- Roboshop/components/common.sh
+++ Roboshop/components/common.sh
@@ -3,7 +3,7 @@ status_check() {
     if [ $1 -eq 0 ]; then
         echo -e "\e[32mSUCCESS\e[0m"
     else
-        echo -e "\e[33mFAILURE\e[0m"
+        echo -e "\e[31mFAILURE\e[0m"
         exit 2
     fi
 } 
