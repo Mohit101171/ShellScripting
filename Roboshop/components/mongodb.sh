05646b742cacac56488e8bddd6f94fd1cfc16ca9
Mohit Kumar <mk.singh0246@gmail.com>
2024-09-08 01:03:26 +0530

    added function in common.sh and used in mongodb.sh

--- Roboshop/components/mongodb.sh
+++ Roboshop/components/mongodb.sh
@@ -1,3 +1,5 @@
+#!bin/bash
+source:components/common.sh
 
 echo "Setting up mongodb repo"
 echo '[mongodb-org-4.2]
@@ -6,26 +8,36 @@ baseurl=https://repo.mongodb.org/yum/redhat/$releasever/mongodb-org/4.2/x86_64/
 gpgcheck=1
 enabled=1
 gpgkey=https://www.mongodb.org/static/pgp/server-4.2.asc' >/etc/yum.repos.d/mongodb.repo
+status_check $?
 
 echo "Installing Mongodb"
 yum install -y mongodb-org &>>/tmp/roboshoplog
+status_check $?
+    
 
 echo "Congifuring mongodb config file to have ip 0.0.0.0"
 sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
+status_check $?
 
 echo "Enabling and starting mongodb service"
 systemctl enable mongod
 systemctl restart mongod
+status_check $?
 
 echo "Donwloading Mongodb schema" 
 curl -s -L -o /tmp/mongodb.zip "https://github.com/roboshop-devops-project/mongodb/archive/main.zip" &>>/tmp/roboshoplog
+status_check $?
 
 cd /tmp
 
 echo "Unzipping mongodb schema archive"
 unzip mongodb.zip &>>/tmp/roboshoplog
 cd mongodb-main
+status_check $?
 
 echo "Loading mongodb schema"
 mongo < catalogue.js &>>/tmp/roboshoplog
 mongo < users.js &>>/tmp/roboshoplog
+status_check $?
+
+exit 0
