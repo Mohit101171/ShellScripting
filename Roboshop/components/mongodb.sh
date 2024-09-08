d01690c64dc4c99bb744bc7408a5bd92842d840f
Mohit Kumar <mk.singh0246@gmail.com>
2024-09-08 13:26:04 +0530

    adding print function in common.sh

--- Roboshop/components/mongodb.sh
+++ Roboshop/components/mongodb.sh
@@ -1,7 +1,7 @@
 #!bin/bash
 source components/common.sh
 
-echo "Setting up mongodb repo"
+print "Setting up mongodb repo"
 echo '[mongodb-org-4.2]
 name=MongoDB Repository
 baseurl=https://repo.mongodb.org/yum/redhat/$releasever/mongodb-org/4.2/x86_64/
@@ -10,32 +10,32 @@ enabled=1
 gpgkey=https://www.mongodb.org/static/pgp/server-4.2.asc' >/etc/yum.repos.d/mongodb.repo
 status_check $?
 
-echo "Installing Mongodb"
+print "Installing Mongodb"
 yum install -y mongodb-org &>>/tmp/roboshoplog
 status_check $?
 
-echo "Congifuring mongodb config file to have ip 0.0.0.0"
+print "Congifuring mongodb config file to have ip 0.0.0.0"
 sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
 status_check $?
 
-echo "Enabling and starting mongodb service"
+print "Enabling and starting mongodb service"
 systemctl enable mongod
 systemctl restart mongod
 status_check $?
 
-echo "Donwloading Mongodb schema" 
+print "Donwloading Mongodb schema" 
 curl -s -L -o /tmp/mongodb.zip "https://github.com/roboshop-devops-project/mongodb/archive/main.zip" &>>/tmp/roboshoplog
 status_check $?
 
 cd /tmp
 
-echo "Unzipping mongodb schema archive"
+print "Unzipping mongodb schema archive"
 unzip -o mongodb.zip &>>/tmp/roboshoplog
 status_check $?
 
 cd mongodb-main
 
-echo "Loading mongodb schema"
+print "Loading mongodb schema"
 mongo < catalogue.js &>>/tmp/roboshoplog
 mongo < users.js &>>/tmp/roboshoplog
 status_check $?
