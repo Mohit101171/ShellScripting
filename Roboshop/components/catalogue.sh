7f7a223f18a62898d869b49b6d5a01c9ad50e367
Mohit Kumar <mk.singh0246@gmail.com>
2024-09-10 15:02:54 +0530

    developing code for catalogue.sh and testing it

--- Roboshop/components/catalogue.sh
+++ Roboshop/components/catalogue.sh
@@ -8,7 +8,7 @@ status_check $?
 print "Adding Roboshop user"
 id roboshop &>>$LOG
 if [ $? -eq 0 ]; then
-    echo "User already exists, skipping."
+    echo "\nUser already exists, skipping."
 else
     useradd roboshop &>>$LOG
 fi 
