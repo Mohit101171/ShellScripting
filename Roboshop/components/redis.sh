c0b471c0992060329417dbf8fd424abaee8c48b3
Mohit Kumar <mk.singh0246@gmail.com>
2024-10-03 13:32:50 +0530

    correcterd redis code

--- Roboshop/components/redis.sh
+++ Roboshop/components/redis.sh
@@ -5,19 +5,19 @@ print "Install yum-utils and download Redis Repos"
 yum install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y &>>$LOG
 status_check $?
 
-print "Setup Redis Repos"
+print "Setup Redis Repos\t\t"
 yum-config-manager --enable remi &>>$LOG
 status_check $?
 
-print "Install Redis"
+print "Install Redis\t\t\t"
 yum install redis -y &>>$LOG
 status_check $?
 
-print "Update the BindIP in redis.conf"
+print "Update the BindIP in redis.conf\t"
 
 sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/redis.conf
 status_check $?
 
-print "Start Redis Service"
+print "Start Redis Service\t\t\"
 systemctl enable redis &>>$LOG && systemctl start redis &>>$LOG
 status_check $?
