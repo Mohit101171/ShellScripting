e01246cbfb03ea2dbcc491d7dd409eac3ddb4412
Mohit Kumar <mk.singh0246@gmail.com>
2024-10-03 13:08:33 +0530

    correcterd redis code

--- Roboshop/components/redis.sh
+++ Roboshop/components/redis.sh
@@ -15,7 +15,7 @@ status_check $?
 
 print "Update the BindIP from 127.0.0.1 to 0.0.0.0 in config file /etc/redis.conf & /etc/redis/redis.conf"
 
-sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/redis.conf
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/redis.conf /etc/redis/redis.conf
 status_check $?
 
 print "Start Redis Service"
