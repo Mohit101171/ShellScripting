b11d41adcd2cf545dfe76ab684fde54a141bc660
Mohit Kumar <mk.singh0246@gmail.com>
2024-11-17 00:04:44 +0530

    making changes to create instance script

--- Roboshop/create-instances.sh
+++ Roboshop/create-instances.sh
@@ -9,6 +9,20 @@ if [ -z "${INSTANCE_NAME}" ]; then
     exit 1
 fi    
 
+aws ec2 describe-instances --filters "Name=tag:Name,Values=${INSTANCE_NAME}" | jq -r .Reservations[].Instances[].State.Name | grep running
+
+if [ $? -eq 0 ]; then
+    echo "$INSTANCE_NAME is already running"
+    exit 0
+fi
+
+aws ec2 describe-instances --filters "Name=tag:Name,Values=${INSTANCE_NAME}" | jq -r .Reservations[].Instances[].State.Name | grep stopped
+
+if [ $? -eq 0 ]; then
+    echo "$INSTANCE_NAME is already provisioned and in stopped state"
+    exit 0
+fi
+
 IP=$(aws ec2 run-instances --launch-template LaunchTemplateId=$LTID,Version=$LTV --tag-specifications "ResourceType=spot-instances-request,Tags=[{Key=Name,Value=$INSTANCE_NAME}]" "ResourceType=instance,Tags=[{Key=Name,Value=$INSTANCE_NAME}]" | jq -r .Instances[].PrivateIpAddress)
 
 echo "$IP"
