dee6141505b2ac61beffe88429a08f2a210b95e8
Mohit Kumar <mk.singh0246@gmail.com>
2024-11-17 00:38:01 +0530

    making changes to create instance script

--- Roboshop/create-instances.sh
+++ Roboshop/create-instances.sh
@@ -12,21 +12,17 @@ fi
 aws ec2 describe-instances --filters "Name=tag:Name,Values=${INSTANCE_NAME}" | jq -r .Reservations[].Instances[].State.Name | grep running &>/dev/null
 
 if [ $? -eq 0 ]; then
-    echo "$INSTANCE_NAME is already running"
+    echo "$INSTANCE_NAME instance is already running"
     exit 0
-else
-    echo "Initiated provisioning $INSTANCE_NAME"
 fi
 
 aws ec2 describe-instances --filters "Name=tag:Name,Values=${INSTANCE_NAME}" | jq -r .Reservations[].Instances[].State.Name | grep stopped &>/dev/null
 
 if [ $? -eq 0 ]; then
-    echo "$INSTANCE_NAME is already provisioned and in stopped state"
+    echo "$INSTANCE_NAME instance is already provisioned and in stopped state"
     exit 0
-else
-    echo "Initiated provisioning $INSTANCE_NAME"
 fi
 
 IP=$(aws ec2 run-instances --launch-template LaunchTemplateId=$LTID,Version=$LTV --tag-specifications "ResourceType=spot-instances-request,Tags=[{Key=Name,Value=$INSTANCE_NAME}]" "ResourceType=instance,Tags=[{Key=Name,Value=$INSTANCE_NAME}]" | jq -r .Instances[].PrivateIpAddress)
 
-echo "$INSTANCE_NAME is provisioned"
+echo "$INSTANCE_NAME instance is provisioned"
