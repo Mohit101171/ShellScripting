4a300c7d5aa9dfa5d10f524c1ceb04b9c9de9a29
Mohit Kumar <mk.singh0246@gmail.com>
2024-11-16 22:25:30 +0530

    making changes to create instance script

--- Roboshop/create-instances.sh
+++ Roboshop/create-instances.sh
@@ -1,12 +1,12 @@
 #!bin/bash
 
-LTID='lt-04bee3717931526a8'
-LTV='$Latest'
-INSTANCE_NAME='$1'
+LTID="lt-04bee3717931526a8"
+LTV="$Latest"
+INSTANCE_NAME="$1"
 
 if [ -z "${INSTANCE_NAME}" ]; then
     echo "Missing name tag input for server creation"
     exit 1
 fi    
 
-IP= $(aws ec2 run-instances --launch-template LaunchTemplateId=$LTID,Version=$LTV --tag-specifications "ResourceType=spot-instances-request,Tags=[{Key=Name,Value=$INSTANCE_NAME}]" "ResourceType=instance,Tags=[{Key=Name,Value=$INSTANCE_NAME}]")
+IP=$(aws ec2 run-instances --launch-template LaunchTemplateId=$LTID,Version=$LTV --tag-specifications "ResourceType=spot-instances-request,Tags=[{Key=Name,Value=$INSTANCE_NAME}]" "ResourceType=instance,Tags=[{Key=Name,Value=$INSTANCE_NAME}]")
