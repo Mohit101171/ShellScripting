2595774367ca58b9b7ef61ef68d1fc5b1b3e1d5d
Mohit Kumar <mk.singh0246@gmail.com>
2024-11-16 22:07:26 +0530

    making changes to create instance script

--- Roboshop/create-instances.sh
+++ Roboshop/create-instances.sh
@@ -2,5 +2,10 @@
 
 LTID='lt-04bee3717931526a8'
 LTV='$Latest'
+INSTANCE_NAME='$1'
 
-IP= aws ec2 run-instances --launch-template LaunchTemplateId=$LTID,Version=$LTV
+if [-z "${INSTANCE_NAME}" ] then
+    exit 1
+fi    
+
+IP= aws ec2 run-instances --launch-template LaunchTemplateId=$LTID,Version=$LTV --tag-specifications 'ResourceType=spot-instances-request",Tags=[{Key=Name,Value=$INSTANCE_NAME}]' 'ResourceType=instance,Tags=[{Key=Name,Value=$INSTANCE_NAME}]'
