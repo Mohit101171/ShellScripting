7b8dc91955068e1042ce9d765dceb2753dc427fd
Mohit Kumar <mk.singh0246@gmail.com>
2025-01-28 13:17:42 +0530

    added code for frontend page

--- Roboshop/components/frontend.sh
+++ Roboshop/components/frontend.sh
@@ -9,17 +9,17 @@ print "Downloading HTDOCS for frontend page"
 curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip" &>>$LOG
 status_check $?
 
-print "Unzip HTDOCS for frontend page"
+print "Unzip HTDOCS for frontend page\t"
 cd /usr/share/nginx && rm -rf * &&  unzip /tmp/frontend.zip &>>$LOG && mv frontend-main/* . && mv static html &>>$LOG 
 status_check $?
 
-print "Copy Nginx ROboshop Config"
+print "Copy Nginx R0boshop Config\t"
 # rm -rf frontend-master static README.md
 mv localhost.conf /etc/nginx/default.d/roboshop.conf
 status_check $?
 
-print "Update Nginx Roboshop Config"
-sed -i -e '/catalogue/ s/localhost/catalogue.roboshop.internal/' etc/nginx/default.d/roboshop.conf &>>$LOG
+print "Update Nginx Roboshop Config\t"
+sed -i -e '/catalogue/ s/localhost/catalogue.roboshop.internal/' /etc/nginx/default.d/roboshop.conf &>>$LOG
 status_check $?
 
 print "Restarting Nginx service"
