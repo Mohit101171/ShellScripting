c471375b0f6f3b52420ac37a3d84ccce22bb90db
Mohit Kumar <mk.singh0246@gmail.com>
2025-01-30 12:55:17 +0530

    updating code to make it dry by creating multiple functions in common.sh

--- Roboshop/components/common.sh
+++ Roboshop/components/common.sh
@@ -20,3 +20,53 @@ fi
 
 LOG=/tmp/roboshop.log
 rm -f $LOG
+
+ADD_APP_USER(){
+    print "Adding Roboshop user"
+    id roboshop &>>$LOG
+    if [ $? -eq 0 ]; then
+        echo -e "\n\e[35mUser already exists, skipping.\e[0m"
+    else
+        useradd roboshop &>>$LOG
+    fi 
+    status_check $?
+}    
+
+SystemD_Setup(){    
+    print "Setting up Mongodb configuration in ${COMPONENT} service"
+    sed -i -e 's/MONGO_DNSNAME/mongodb.roboshop.internal/' /home/roboshop/${COMPONENT}/systemd.service &>>$LOG && mv /home/roboshop/${COMPONENT}/systemd.service /etc/systemd/system/${COMPONENT}.service
+    status_check $?
+
+    print "Setting up and starting ${COMPONENT} service"
+    systemctl daemon-reload && systemctl restart ${COMPONENT} && systemctl enable ${COMPONENT}
+    status_check $?
+}
+
+DOOWNLOAD(){
+    print "Download ${COMPONENT} components"
+    curl -s -L -o /tmp/${COMPONENT}.zip "https://github.com/roboshop-devops-project/${COMPONENT}/archive/main.zip" &>>$LOG
+    status_check $?
+
+    cd /home/roboshop
+    print "Unzip ${COMPONENT} components"
+    rm -rf ${COMPONENT} && unzip -o /tmp/${COMPONENT}.zip &>>$LOG && mv ${COMPONENT}-main ${COMPONENT}
+    status_check $?
+}
+
+COMPONENT(){
+    print "Installing NodeJS"
+    yum install nodejs make gcc-c++ -y &>>$LOG
+    status_check $?
+
+    ADD_APP_USER
+    DOOWNLOAD
+    
+    cd /home/roboshop/${COMPONENT}
+    print "Download NodeJS dependencies"
+    npm install --unsafe-perm &>>$LOG
+    status_check $?
+
+    chown roboshop:roboshop -R /home/roboshop
+
+    SystemD_Setup
+}
